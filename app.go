8dd384589520442ad0789ef669251c796fdfc73a
ajax997 <nguyennghi1997@outlook.com>
2022-01-21 16:10:29 +0700

    init

---
+++ app.go
@@ -0,0 +1,32 @@
+// app.go
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString :=
+		fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s", user, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+}
+
+func (a *App) Run(addr string) {}
